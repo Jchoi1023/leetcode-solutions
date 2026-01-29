1cb0b6c6196ec3670609626cf467020b4f7085b4
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-01-29 13:16:53 -0600

    Time: 699 ms (39.26%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
+
+
+
 select department, employee, salary 
 from(
 select d.name as department, e.name as employee, e.salary as salary,
@@ -6,5 +9,4 @@ RANK() OVER (PARTITION BY d.id ORDER BY e.salary DESC) AS rnk
 from employee e
 left join department d on e.departmentid = d.id
 ) t
-WHERE rnk = 1;
-
+where rnk = 1
