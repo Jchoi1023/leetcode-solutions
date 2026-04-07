12a1b64ba1fababe0958385a9ca7eaea9a1673a7
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-04-06 22:11:56 -0500

    Time: 930 ms (62.05%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 select  st.student_id, st.student_name, sb.subject_name, count(ex.student_id) as attended_exams
 from students st
-cross join subjects sb
-left join examinations ex on sb.subject_name = ex.subject_name 
+cross join subjects sb #to ensure all combinations exist, including zero attendance
+left join examinations ex on sb.subject_name = ex.subject_name #to keep all student-subject pairs even with no exam records
                             and st.student_id = ex.student_id
 group by st.student_id, st.student_name, sb.subject_name
 order by st.student_id, st.student_name asc
