1c95765d8afde0d38122f32aa9952257751379bb
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-04-06 20:27:36 -0500

    Time: 404 ms (74.16%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
 select w1.id
 from weather w1
-join weather w2 on DATEDIFF(w1.recordDate, w2.recordDate) = 1
-where w1.temperature > w2.temperature
-
+left join weather w2 on (w2.recordDate + interval 1 day) = w1.recordDate
+where w1.temperature - w2.temperature > 0
