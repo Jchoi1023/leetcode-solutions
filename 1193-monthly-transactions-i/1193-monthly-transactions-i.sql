a105133e4d2f8a888fb4eb1487050d6349600523
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-04-07 23:36:12 -0500

    Time: 559 ms (65.3%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -4,6 +4,6 @@ country,
 count(state) as trans_count, 
 sum(CASE WHEN state = 'approved' then 1 else 0 end) as approved_count,
 sum(amount) as trans_total_amount,
-sum((CASE WHEN state = 'approved' then 1 else 0 end) * amount) as approved_total_amount
+sum(CASE WHEN state = 'approved' then amount else 0 end) as approved_total_amount
 FROM transactions
 group by month, country;
