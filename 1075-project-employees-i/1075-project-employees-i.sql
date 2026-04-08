dbee78169530bcb9340444694c434132c578df11
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-04-07 21:21:20 -0500

    Time: 491 ms (61.66%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,4 +1,10 @@
 # Write your MySQL query statement below
-select p.project_id, round(avg(e.experience_years),2) as average_years from project p
-join employee e on p.employee_id = e.employee_id
-group by p.project_id
+with new_table as (
+    select p.project_id as project_id, e.experience_years as years, e.name as name
+    from project p
+    left join employee e on p.employee_id = e.employee_id
+)
+
+select project_id, round(sum(years)/count(name),2) as average_years
+from new_table
+group by project_id
