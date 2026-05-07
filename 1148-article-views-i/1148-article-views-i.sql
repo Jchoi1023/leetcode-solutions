44750494c5be4544945ab4e2492408e580500e6e
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-05-07 07:20:20 -0500

    Time: 448 ms (24.62%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,5 +1,29 @@
 # Write your MySQL query statement below
-select distinct author_id as id
-from views
-where author_id = viewer_id
-order by author_id asc
+-- all the authors that viewed at least one of their articles.
+-- sorting id asc
+
+-- author_id == viewer_id 
+-- count(article_id) >= 1
+
+-- we cannot use aggregate function with where, because filters rows happens before 
+-- aggregate function so I create WITH 
+
+WITH cnt as (
+    select 
+        distinct author_id as id, 
+        author_id, 
+        viewer_id,
+        count(author_id) as count_article
+    from views
+    where author_id = viewer_id
+    group by author_id, viewer_id
+)
+
+select 
+id
+from cnt
+where count_article >= 1
+order by id asc;
+
+
+
