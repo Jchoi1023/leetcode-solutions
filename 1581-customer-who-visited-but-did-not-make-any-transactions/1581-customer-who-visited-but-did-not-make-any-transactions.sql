e03a40a59f32623ed9fa1d799c83fba8d2f72434
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-05-07 15:49:47 -0500

    Time: 1342 ms (51.5%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,6 +1,22 @@
 # Write your MySQL query statement below
-select v.customer_id, count(v.visit_id) as count_no_trans
-from visits v
-left join transactions t on v.visit_id = t.visit_id
-where t.transaction_id is NULL
-group by v.customer_id
+
+# if they visited twice a day, then they had a different id?
+# no transactions and how many times did they do?
+# no tran_id means no visited_id?
+
+WITH ctn AS (
+    select v.customer_id as customer_id, 
+        t.visit_id as visited_id,
+        t.transaction_id
+    from visits v
+    left join transactions t on v.visit_id = t.visit_id 
+    where t.transaction_id is NULL
+
+)
+
+select
+    customer_id, count(customer_id) as count_no_trans
+from ctn
+group by customer_id
+
+
