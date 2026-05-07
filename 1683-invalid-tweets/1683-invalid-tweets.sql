9be8105ae92ea2047fa03ece79c7e1f7422b2e99
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-05-07 07:28:58 -0500

    Time: 831 ms (6.78%), Space: 0B (100%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.sql
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -1,4 +1,11 @@
 # Write your MySQL query statement below
+
+-- find ID
+-- number of characters in the content > 15, not special characters.
+
+-- Length, Replace
+
+
 select tweet_id
 from tweets
-where length(content) > 15
+where length(replace(content, ' ', '')) > 15
