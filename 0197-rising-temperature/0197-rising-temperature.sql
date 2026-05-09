2bb40951c59c945c30ed3219979fa9e90800dd3e
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-05-09 18:59:42 -0500

    Time: 416 ms (76.93%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,16 @@
 # Write your MySQL query statement below
-select w1.id
+
+# higher temp compared to its previous dates
+# join with the same table to compare temp with the date
+
+
+select 
+    w2.id
+    -- w1.recorddate,
+    -- w2.recorddate,
+    -- w1.temperature,
+    -- w2.temperature, 
+    -- w1.id
 from weather w1
-left join weather w2 on w1.recordDate = Date_add(w2.recordDate, interval 1 day)
-where w1.temperature > w2.temperature
+left join weather w2 ON DATE_ADD(w1.recordDate, INTERVAL 1 DAY) = w2.recordDate
+where w1.temperature < w2.temperature
