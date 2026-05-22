167388ea8f0a12f4ff3c1c5cbd3aa9dc4f51e442
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-05-21 20:05:27 -0700

    Time: 566 ms (52.97%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,14 +1,19 @@
 # Write your MySQL query statement below
-with first_login as (
-    select player_id, MIN(event_date) as first_date
-    from activity
-    group by player_id
+# fraction = player logged in again on the day after the day they first logged in
+# divide it by the number of total players
+with first_order as
+(
+SELECT *
+FROM (
+    SELECT *, 
+           ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY event_date) AS rn 
+    FROM activity
+) t
+WHERE rn = 1
 )
 
-select ROUND(
-        COUNT(DISTINCT a.player_id) * 1.0 / COUNT(DISTINCT f.player_id),
-        2
-    ) AS fraction
-from first_login f
-left join activity a on f.player_id = a.player_id
-and a.event_date = date_add(f.first_date, interval 1 day)
+select  round(count(a.event_date) /count(f.event_date),2) as fraction
+from first_order f 
+left join activity a 
+ON DATE_ADD(f.event_date, INTERVAL 1 DAY) = a.event_date
+and f.player_id = a.player_id
