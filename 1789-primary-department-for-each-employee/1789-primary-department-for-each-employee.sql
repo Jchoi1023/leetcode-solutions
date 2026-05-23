ea8ec3425059e48511d0514a01cd92200221d733
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-05-23 14:25:26 -0700

    Time: 564 ms (47%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,10 +1,19 @@
 # Write your MySQL query statement below
-WITH cnt AS (
-    SELECT *,
-           COUNT(*) OVER (PARTITION BY employee_id) AS cnt
+
+# employee_id = department_id -> department_ id is primary
+# employee_id != department_id -> primary flag Y 
+
+WITH cte AS
+(
+    SELECT
+        employee_id,
+        CASE
+            WHEN COUNT(*) = 1 THEN MAX(department_id)
+            WHEN COUNT(*) > 1 THEN MAX(CASE WHEN primary_flag = 'Y' THEN department_id END)
+        END AS department_id
     FROM employee
+    GROUP BY employee_id
 )
-SELECT employee_id, department_id
-FROM cnt
-WHERE (cnt >= 2 AND primary_flag = 'Y')
-   OR (cnt = 1 AND primary_flag = 'N');
+
+SELECT * FROM cte
+WHERE department_id IS NOT NULL;
