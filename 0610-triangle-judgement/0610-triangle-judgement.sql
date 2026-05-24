9080eca597a225356aa6f6f1490e6e8a46bd9cef
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-05-23 18:30:54 -0700

    Time: 312 ms (27.92%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,8 +1,10 @@
 # Write your MySQL query statement below
-select x, y, z, 
-case
-    when x <= 0 or y <= 0 or z <=0 then 'No'
-    when greatest(x,y,z) < (x+y+z-greatest(x,y,z)) then 'Yes' 
-    else 'No'
-end as triangle
-from triangle;
+
+select 
+    x, 
+    y,
+    z,
+    CASE
+        WHEN x + y + z - GREATEST(x, y, z) > GREATEST(x, y, z) THEN 'Yes' ELSE 'No'
+    END AS triangle
+from triangle
