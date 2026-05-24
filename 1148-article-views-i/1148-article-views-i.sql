32442292095962da7e2be2bcab969443ffbfd719
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-05-23 19:55:52 -0700

    Time: 407 ms (72.55%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,19 +1,6 @@
 # Write your MySQL query statement below
-# author_id = viewer_id
-# count(author_id) >= 1
 
-
-with cnt as (
-    select 
-        count(author_id),
-        author_id,
-        viewer_id
-    from views
-    group by author_id, viewer_id
-)
-
-select 
-    distinct author_id as id
+select distinct author_id as id
 from views
 where author_id = viewer_id
-order by id asc;
+order by author_id
