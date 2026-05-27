f9904bd88bff2a86e9774c931bc052c5e8c5da51
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-05-27 16:36:18 -0700

    Time: 348 ms (62.12%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,14 +1,9 @@
 # Write your MySQL query statement below
 
-# managers with at least five direct reports
-# any order
+# five direct reports
 
-with cnt as(
-    select e1.id, e1.name as name, count(e2.id) as count
+select e1.name
 from employee e1
-join employee e2 on e1.id = e2.managerId
-group by e2.managerid
-)
-select name 
-from cnt
-where count >= 5
+left join employee e2 on e1.id = e2.managerId
+group by e1.id
+having count(e1.id) >= 5
