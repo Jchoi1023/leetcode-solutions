87f1b753ff87b0591637ebf9ae894398e02eac87
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-05-27 17:01:52 -0700

    Time: 665 ms (59.85%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,10 +1,7 @@
 # Write your MySQL query statement below
 
-# use case to count number of confirmed and timeout
-
-select 
-    s.user_id, 
-   coalesce(round(sum(case when c.action = 'confirmed' then 1 else 0 end)/count(c.action),2), 0) as confirmation_rate
+select s.user_id,
+coalesce(round(SUM(CASE WHEN c.action = 'confirmed' then 1 else 0 end)/count(c.user_id), 2),0) as confirmation_rate
 from signups s
 left join confirmations c on s.user_id = c.user_id
 group by user_id
