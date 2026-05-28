ed43e1fcb069ea65a738e9783cf633df07bc28d4
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-05-27 17:20:29 -0700

    Time: 801 ms (62.77%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,16 +1,9 @@
 # Write your MySQL query statement below
 
-#purchase data between start and end date
---  p.start_date, p.end_date, p.price, u.purchase_date, u.units,
-with cal as (
-    select p.product_id, (p.price * u.units) as cal , u.units
--- (p.price*u.units)/sum(u.units) as average_price
-from prices p
-left join unitssold u on p.product_id = u.product_id 
-and u.purchase_date between p.start_date and p.end_date
-group by p.product_id, u.purchase_date
-)
+# join the two table to calculate average price
 
-select product_id, coalesce(round(sum(cal)/sum(units),2),0) as average_price
-from cal
-group by product_id
+select p.product_id, coalesce(round(sum(p.price*u.units)/sum(u.units),2),0) as average_price
+from prices p
+left join unitssold u on p.product_id = u.product_id
+and u.purchase_date between p.start_date and p.end_date
+group by p.product_id
