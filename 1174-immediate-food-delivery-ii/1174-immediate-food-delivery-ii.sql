0d54bdef139f2f6d0147fbc81f272278b0f11180
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-05-30 18:22:46 -0500

    Time: 587 ms (90.26%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,20 +1,18 @@
 # Write your MySQL query statement below
+# pref delivery date = the order date -> immediate, otherwise -> schedule
+# first order.
 
-# immediate : order_date = pref_delivery_date, otherwise : scheduled
+#rank() over to get the first order
 
-# count all first order
-# count first order and immediate
-with count as (
-select count(*) as cnt
-from (
-    select *, 
-    row_number() over (partition by customer_id order by order_date) as rn 
-    from delivery) t
-where rn = 1
-and order_date = customer_pref_delivery_date
+WITH cte AS (
+    SELECT customer_id, order_date, customer_pref_delivery_date,
+           ROW_NUMBER() OVER (
+               PARTITION BY customer_id
+               ORDER BY order_date
+           ) AS rn
+    FROM delivery
 )
 
-select round((select cnt from count)/count( distinct customer_id)*100,2) as immediate_percentage
-from delivery
-
-
+select 
+round(sum(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END)/count(customer_id)*100,2) as immediate_percentage from cte
+where rn = 1
