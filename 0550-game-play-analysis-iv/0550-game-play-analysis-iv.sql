7f1516e7d7b682609faf693fe36a696a11511f7a
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-05-30 18:47:46 -0500

    Time: 538 ms (82.53%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,21 +1,19 @@
 # Write your MySQL query statement below
-
-# count the number of total players
-# logged in again on the day after the day first logged in -> fraction
-with cnt as
-(
-select count(distinct t.player_id) as cnt
-from
-(
-select a2.player_id, row_number() over (partition by a1.player_id order by a1.event_date) as rn
-from activity a1
-left join activity a2 on DATE_ADD(a1.event_date, interval 1 day) = a2.event_date 
-and a1.player_id = a2.player_id
-)t
-where rn =1
-
+# self joint with the date and player_id
+with cte as (
+    select a1.player_id as total_player, 
+    a1.event_date as event, 
+    a2.player_id as player, 
+    a2.event_date as date,
+        ROW_NUMBER() OVER (
+            PARTITION BY a1.player_id
+            ORDER BY a1.event_date
+           ) AS rn    
+    from activity a1
+    left join activity a2 on DATE_ADD(a1.event_date, interval 1 day) = a2.event_date
+    and a1.player_id = a2.player_id
 )
 
-select round((select cnt from cnt)/count(distinct player_id),2) as fraction
-from activity
-
+select round(count(distinct player)/count(distinct total_player),2) as fraction
+from cte
+where rn = 1
