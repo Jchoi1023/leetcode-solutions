daccd824750edd897ad05aa183691bdb6d51d67a
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-01 10:46:41 -0500

    Time: 514 ms (10.91%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,12 +1,11 @@
 # Write your MySQL query statement below
-with max as
-(
-select n1.num as number, count(n2.num)
-from mynumbers n1
-join mynumbers n2 on n1.num = n2.num
-group by n2.num
-having count(n2.num) = 1
+with cte as (
+select m1.num as num, count(m2.num) as cnt
+from mynumbers m1
+left join mynumbers m2 on m1.num = m2.num
+group by m1.num
 )
 
-select max(number) as num
-from max
+select coalesce(max(num), null) as num
+from cte
+where cnt = 1
