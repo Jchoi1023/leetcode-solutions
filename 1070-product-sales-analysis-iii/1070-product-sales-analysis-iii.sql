2d0cd7727a2a2cbb53086b801c6291534d0afea9
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-01 10:33:44 -0500

    Time: 716 ms (53.7%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,13 +1,15 @@
 # Write your MySQL query statement below
-with first_year as (
-select * 
-from(
-    select product_id, year as first_year, quantity, price,
-        dense_rank() over (partition by product_id order by year) as drn
+
+# first year each product was sold
+# create cte for finding the first year 
+# And then filtering with this 
+
+with cte as(
+    select product_id, quantity, price, year as first_year,
+    dense_rank() over(partition by product_id order by year) as rn 
     from sales
-)t
-    where drn = 1
-)
+) 
 
 select product_id, first_year, quantity, price
-from first_year
+from cte
+where rn = 1
