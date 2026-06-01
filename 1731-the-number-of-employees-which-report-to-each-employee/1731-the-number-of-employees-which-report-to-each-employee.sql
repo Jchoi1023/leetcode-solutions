638e6c21d78934e9af6b023fe1d63da4c7db65b1
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-01 11:14:49 -0500

    Time: 654 ms (49.5%), Space: 0B (100%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,10 +1,13 @@
 # Write your MySQL query statement below
 
-# manager who have at least 1 employee
-# some employee do not report to anyone
+# manager who has at least 1 other employee
+# ids, names of manager, the number of employees who report directly to them, 
+# average age of the reports rounded to the nearst integer.
 
-select e1.employee_id, e1.name, count(e2.employee_id) as reports_count, round(avg(e2.age),0) as average_age
+select e1.employee_id, e1.name,
+count(e2.employee_id) as reports_count, round(avg(e2.age),0) as average_age
 from employees e1
-join employees e2 on e1.employee_id = e2.reports_to
-group by e1.employee_id
+left join employees e2 on e1.employee_id = e2.reports_to
+where e2.reports_to is not null
+group by e2.reports_to
 order by e1.employee_id
