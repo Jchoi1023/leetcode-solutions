0931aac73d35e921bca3db5a894163b4cddc3e0a
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-03 14:28:35 -0500

    Time: 565 ms (58.54%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,14 +1,9 @@
 # Write your MySQL query statement below
-
-# at least three times consecutively
-# LAG() lets you look at previous rows without doing a self join
-
 select distinct num as ConsecutiveNums
-    from(
-        select
-        num, 
-        LAG(num, 1) over (order by (id)) as prev1,
-        LAG(num, 2) over (order by (id)) as prev2
-        from logs
-    ) t
-where num = prev1 and num = prev2
+from (
+    select num,
+    LAG(num, 1) over (order by id) as prev1,
+    LAG(num, 2) over (order by id) as prev2
+    from logs
+) t
+where prev1 = num and prev2 = num
