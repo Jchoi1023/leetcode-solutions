6b5a0c1919e3949a28910e61ba83cf4fe1354ca4
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-03 13:58:09 -0500

    Time: 622 ms (18.93%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,19 +1,8 @@
 # Write your MySQL query statement below
 
-# employee_id = department_id -> department_ id is primary
-# employee_id != department_id -> primary flag Y 
-
-WITH cte AS
-(
-    SELECT
-        employee_id,
-        CASE
-            WHEN COUNT(*) = 1 THEN MAX(department_id)
-            WHEN COUNT(*) > 1 THEN MAX(CASE WHEN primary_flag = 'Y' THEN department_id END)
-        END AS department_id
+select employee_id,
+case when count(*) = 1 then department_id
+     when count(*) > 1 then MAX(CASE WHEN primary_flag = 'Y' THEN department_id END)
+      END AS department_id
     FROM employee
     GROUP BY employee_id
-)
-
-SELECT * FROM cte
-WHERE department_id IS NOT NULL;
