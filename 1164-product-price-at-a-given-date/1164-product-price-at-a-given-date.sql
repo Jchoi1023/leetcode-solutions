cc436d4c717c72656fed7ff4a8e052d5adcf10e5
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-03 14:54:31 -0500

    Time: 470 ms (72.63%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,17 +1,22 @@
 # Write your MySQL query statement below
 
-# find the prices of all products on the date 2019-08-16
-WITH cte as 
-(
-select product_id, new_price, change_date
-from products
-where (product_id, change_date) IN (
-    select product_id, MAX(change_date)
-    from products
-    where change_date <= '2019-08-16'
-    group by product_id)
-    )
+SELECT product_id, new_price AS price
+FROM (
+    SELECT
+        product_id,
+        new_price,
+        ROW_NUMBER() OVER (
+            PARTITION BY product_id
+            ORDER BY change_date DESC
+        ) AS rn
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+) t
+WHERE rn = 1
 
+UNION
 
-select distinct p1.product_id, CASE WHEN c.new_price IS NOT NULL then c.new_price else 10 end as price from products p1
-left join cte c on p1.product_id = c.product_id
+SELECT product_id, 10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16';
