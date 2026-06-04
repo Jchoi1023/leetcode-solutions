398d0e1dd8205d0019411869c835df6ceec2f3ee
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-04 18:42:58 -0500

    Time: 464 ms (79.13%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,19 +1,32 @@
 # Write your MySQL query statement below
 
-SELECT product_id, new_price AS price
-FROM (
-    SELECT
-        product_id,
-        new_price,
-        ROW_NUMBER() OVER (
-            PARTITION BY product_id
-            ORDER BY change_date DESC
-        ) AS rn
-    FROM Products
-    WHERE change_date <= '2019-08-16'
-) t
-WHERE rn = 1
+# need to get the update price before 2019-08-16, Subquery
+# price change after 2019-08-16 then the price will be 10 even though it has been changed.
 
+select product_id, new_price as price
+from (
+    select 
+    product_id, 
+    new_price,
+    ROW_NUMBER() OVER (partition by product_id order by change_date desc) as rn
+
+from products
+where change_date <= '2019-08-16' 
+) t
+where rn = 1
+-- SELECT product_id, new_price AS price
+-- FROM (
+--     SELECT
+--         product_id,
+--         new_price,
+--         ROW_NUMBER() OVER (
+--             PARTITION BY product_id
+--             ORDER BY change_date DESC
+--         ) AS rn
+--     FROM Products
+--     WHERE change_date <= '2019-08-16'
+-- ) t
+-- WHERE rn = 1
 UNION
 
 SELECT product_id, 10 AS price
