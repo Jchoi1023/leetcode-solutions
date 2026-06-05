de53d7cbb90d3191228e51816a14051c38d2da91
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-05 16:24:26 -0500

    Time: 472 ms (71.75%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,35 +1,25 @@
 # Write your MySQL query statement below
 
-# need to get the update price before 2019-08-16, Subquery
-# price change after 2019-08-16 then the price will be 10 even though it has been changed.
+# price changed before 2019-08-16 
+# changed after 2019-08-16, then 10
+
+WITH cte AS (
+    SELECT *,
+           ROW_NUMBER() OVER(
+               PARTITION BY product_id
+               ORDER BY change_date DESC
+           ) AS rn
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+) 
 
 select product_id, new_price as price
-from (
-    select 
-    product_id, 
-    new_price,
-    ROW_NUMBER() OVER (partition by product_id order by change_date desc) as rn
+from cte
+where rn = 1 
 
-from products
-where change_date <= '2019-08-16' 
-) t
-where rn = 1
--- SELECT product_id, new_price AS price
--- FROM (
---     SELECT
---         product_id,
---         new_price,
---         ROW_NUMBER() OVER (
---             PARTITION BY product_id
---             ORDER BY change_date DESC
---         ) AS rn
---     FROM Products
---     WHERE change_date <= '2019-08-16'
--- ) t
--- WHERE rn = 1
 UNION
 
-SELECT product_id, 10 AS price
-FROM Products
-GROUP BY product_id
-HAVING MIN(change_date) > '2019-08-16';
+select product_id, 10 as price
+from products
+group by product_id
+having min(change_date) > '2019-08-16'
