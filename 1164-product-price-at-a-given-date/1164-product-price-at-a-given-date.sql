14f0e20dd4df7634f4948c0859f8d4736514ced6
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-06 11:32:44 -0500

    Time: 552 ms (34.4%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,21 +1,19 @@
 # Write your MySQL query statement below
 
-# price changed before 2019-08-16 
+# some product was changed to a new price at some date.
+# all products have price 10
+# on the date 2019-08-16
+# any order
+
+# latest price change before 2019-08-16
 # changed after 2019-08-16, then 10
-
-WITH cte AS (
-    SELECT *,
-           ROW_NUMBER() OVER(
-               PARTITION BY product_id
-               ORDER BY change_date DESC
-           ) AS rn
-    FROM Products
-    WHERE change_date <= '2019-08-16'
-) 
-
-select product_id, new_price as price
-from cte
-where rn = 1 
+select product_id, new_price as price from (
+    select product_id, new_price,
+    ROW_NUMBER() over (partition by product_id order by change_date desc) as rn
+    from products
+    where change_date <= '2019-08-16'
+) t
+where rn = 1
 
 UNION
 
@@ -23,3 +21,7 @@ select product_id, 10 as price
 from products
 group by product_id
 having min(change_date) > '2019-08-16'
+
+
+
+
