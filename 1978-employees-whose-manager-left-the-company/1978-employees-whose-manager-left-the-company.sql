30ebb9cbefd317135a334251a5d43d359e911c44
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-06 12:14:25 -0500

    Time: 365 ms (33.83%), Space: 0B (100%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -1,18 +1,16 @@
 # Write your MySQL query statement below
--- select e.employee_id from employees e
--- where e.salary < 30000 and e.manager_id is not null
--- and not exists (
---     select 1
---     from employees m
---     where m.employee_id = e.manager_id
--- )
 
-select e.employee_id
-from employees e
-left join employees m on m.employee_id = e.manager_id
-where m.employee_id is null and e.salary < 30000 and e.manager_id IS NOT NULL
-order by e.employee_id asc;
+# find the id, salary < 30000 and whose manager left the coompany
+# manager who leaves, deleted from employee table, but report still have their manager_id.
+with cte as(
+select *
+from employees
+WHERE manager_id NOT IN (
+    SELECT employee_id
+    FROM employees
+))
 
--- select *
--- from employees e
--- left join employees m on m.employee_id = e.manager_id
+select employee_id 
+from cte
+where salary < 30000
+order by employee_id
