a14a0bc430afad271dfecf040a6fad3770274a46
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-08 17:09:52 -0500

    Time: 1298 ms (71.01%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,22 +1,30 @@
 # Write your MySQL query statement below
 
-# need to find the user who has rated the greatest number of movies
-# need to fine movie name with the highest average rating in February 2020
+# Need to find two differet things
+# The name of the user who has rated the greates number of movies
+# The movie name with the highest average rating in Feb 2020
 
-# first, I need to join the tables.
-with cte as (
-select m.movie_id, m.title, u.user_id, u.name,
-mr.rating, mr.created_at
-from movies m
+# First of all, I need to join all the table to get the data
+# Need separate queries for both because two different “top 1” problems with different grouping rules
+with cte as(
+select 
+    m.movie_id,
+    m.title,
+    u.user_id, 
+    u.name,
+    mr.rating,
+    mr.created_at
+from movies m 
 left join movierating mr on m.movie_id = mr.movie_id
-left join users u on u.user_id = mr.user_id
+left join users u on mr.user_id = u. user_id
 ),
 
 user_result as (
 select name as results
 from cte
 group by user_id
-order by count(movie_id) desc, name asc
+-- having count(*) 
+order by count(*) desc, name asc
 limit 1
 ),
 
@@ -24,11 +32,13 @@ movie_result as (
     select title as results
     from cte
     where created_at between '2020-02-01' and '2020-02-29'
-    group by movie_id
+    group by title
     order by avg(rating) desc, title asc
-    limit 1
+    limit  1
 )
 
 select results from user_result
+
 UNION ALL
+
 select results from movie_result
