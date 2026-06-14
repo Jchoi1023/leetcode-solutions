46241b16456c2de91899f0f0322c4cdca542a483
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-13 20:09:58 -0500

    Time: 545 ms (93.02%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,21 +1,16 @@
 # Write your MySQL query statement below
 
-# the sum or all total investment values in 2016 tiv_2016 for all policyholders:
-# have the same tiv_2015 values as one or more other policyholders
-# not located in the same city as any other policyholder
-
-SELECT 
-    ROUND(SUM(tiv_2016), 2) AS tiv_2016
-FROM Insurance
-WHERE (lat, lon) IN (
-    SELECT lat, lon
-    FROM Insurance
-    GROUP BY lat, lon
-    HAVING COUNT(*) = 1
+select round(sum(tiv_2016),2) as tiv_2016
+from insurance
+where tiv_2015 in (
+    select tiv_2015 
+    from insurance 
+    group by tiv_2015 
+    having count(*)>1)
+and 
+(lat, lon) in (
+    select lat, lon
+    from insurance
+    group by lat, lon
+    having count(*) = 1
 )
-AND tiv_2015 IN (
-    SELECT tiv_2015
-    FROM Insurance
-    GROUP BY tiv_2015
-    HAVING COUNT(*) > 1
-);
