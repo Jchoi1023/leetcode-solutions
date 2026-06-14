10ece27622cbef216c70f39be10873167ff4f914
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-14 09:48:20 -0500

    Time: 774 ms (39.5%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,7 +1,14 @@
 # Write your MySQL query statement below
-select p.product_name, sum(o.unit) as unit
-from products p
-left join orders o on p.product_id = o.product_id
-where o.order_date between '2020-02-01' and '2020-02-29'
-group by p.product_name
-having sum(o.unit) >= 100;
+
+# name of products that have at lease 100 units ordered in Feb 2020
+with cte as (
+    select p.product_id, p.product_name, p.product_category, o.order_date, o.unit
+    from products p 
+    left join orders o on p.product_id = o.product_id
+    where o.order_date between '2020-02-01' and '2020-02-29'
+)
+
+select product_name, sum(unit) as unit
+from cte
+group by product_name
+having sum(unit) >= 100
