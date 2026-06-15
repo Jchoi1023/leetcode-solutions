bae7d359b6adaf338d87ce6235c86e9cae3225d9
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-15 16:53:24 -0500

    Time: 587 ms (38.04%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,12 +1,13 @@
 # Write your MySQL query statement below
-# primary department for each employee
+# emplyee_id = department_id if they belong to only one dept.
 
-# count(*) = 1, then department_id,
-# count(*) > 1, then to find primary_flag = 'Y'
+select employee_id,  department_id
+from Employee
+where primary_flag = "Y"
 
-select employee_id, 
-CASE WHEN count(*) = 1 THEN department_id
-    when count(*) > 1 then MAX(CASE WHEN primary_flag = 'Y' THEN department_id END)
-    END AS department_id
+UNION
+
+select employee_id, department_id
 from employee
 group by employee_id
+having count(*) = 1
