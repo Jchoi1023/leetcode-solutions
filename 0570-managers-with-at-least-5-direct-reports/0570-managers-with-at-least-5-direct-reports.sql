967b6514f46e0d90c82d477cf0e0142822f57dc3
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-16 20:59:22 -0500

    Time: 338 ms (88.71%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,9 +1,16 @@
 # Write your MySQL query statement below
 
-select 
-e2.name
-from employee e1
-join employee e2 on e1.managerId = e2.id
-group by e1.managerID
-having count(*) >= 5
+# Let me make sure that I understand the problem correctly.
+# So I need to find managers with at least five direct reports, right?
 
+-- select name, count(*)
+-- from employee 
+-- group by managerId
+# I'm going to self join the table because the aggregation to show employee name not the manager's name, so to show the manager's name, I'm going to self join the table.
+
+select a1.name
+from employee a1
+join employee a2 on a1.id = a2.managerId
+# I joined with id and managerId so that I can get the employee list with their manager.
+group by a2.managerId
+having count(a2.id) >= 5
