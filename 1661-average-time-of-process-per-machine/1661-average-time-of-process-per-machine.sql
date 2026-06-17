73b1ee5d62a6c7cafdaaf2ab566de5f8f4cd9456
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-16 20:40:28 -0500

    Time: 253 ms (64.74%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,18 +1,14 @@
 # Write your MySQL query statement below
 
-# need to find average time. 
-# end - start
-
-with cte as (
-    select a1.machine_id, a1.process_id, a1.activity_type, a1.timestamp as start, a2.timestamp as end
+# Let me make sure I understand the problem correctly.
+# Each process has exactly one start record and one end record so that I can calcuate the average?
+# Since the start and end timestamps are stored separate rows, so I do join them to bring into the same row.
+# Alias 
+select a1.machine_id as machine_id,round(avg(a2.timestamp - a1.timestamp), 3) as processing_time
 from activity a1
-left join activity a2 on a1.machine_id = a2.machine_id 
-and a1.process_id = a2.process_id
+join activity a2 on a1.machine_id = a2.machine_id
+and a1.process_id = a2.process_id 
+# with this self join, it would also match start records with other start records and end records with other end records because I'm only using joining on machine_id and process_id.
 where a1.activity_type = 'start' and a2.activity_type = 'end'
-)
-
-
-
-select machine_id, round(avg(end - start),3) as processing_time
-from cte
-group by machine_id
+# we need to group by since the requirement is for calculating average processing time by machine.
+group by a1.machine_id
