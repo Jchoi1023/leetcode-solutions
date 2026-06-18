c0cb510e64aa2da32d48d159937151fb4a0632b2
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-17 21:44:41 -0500

    Time: 523 ms (96.72%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,11 +1,18 @@
 # Write your MySQL query statement below
-with cte as(
-select num,
-LAG(num, 1) OVER(order by id) as prev1,
-LAG(num, 2) OVER(order by id) as prev2
-from logs
-)
+
+# Let me make sure that I understand correctly.
+# number appear at least three times consecutively.
+
+# LAG returns previous row value, so I need to compare current row and previous, and previous row and before that. window function
+# LAG if there is no value in the previous two rows, it returns NULL, not error.
+with cte as (
+    select num,
+    LAG(num) over (order by id) as pre_num,
+    LAG(num, 2) over (order by id) as pre_before_num
+    from logs
+)   
 
 select distinct num as ConsecutiveNums
 from cte
-where num = prev1 and num = prev2
+where num = pre_num
+and pre_num = pre_before_num
