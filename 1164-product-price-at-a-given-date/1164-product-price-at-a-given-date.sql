24199736f52b5e8c871aaa53864848944fd637d9
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-21 16:13:00 -0500

    Time: 455 ms (92.46%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,27 +1,27 @@
 # Write your MySQL query statement below
 
-# some product was changed to a new price at some date.
-# all products have price 10
-# on the date 2019-08-16
-# any order
+# Let me clarify that I understand this questio correctly.
+# So I need to get new price which updated before 2019-08-16.
+# Price updated after 2019-08-16, should be 10.
 
-# latest price change before 2019-08-16
-# changed after 2019-08-16, then 10
-select product_id, new_price as price from (
-    select product_id, new_price,
-    ROW_NUMBER() over (partition by product_id order by change_date desc) as rn
-    from products
-    where change_date <= '2019-08-16'
-) t
+# UNION - combine results
+# product price updated before 2019-08-16
+# product price 10 which didn't update the price before 2019-08-16
+
+with cte as (
+  select product_id, new_price as price, change_date,
+ROW_NUMBER() OVER (partition by product_id order by change_date desc) as rn
+from products  
+where change_date <= "2019-08-16"
+)
+
+select product_id, price
+from cte
 where rn = 1
 
-UNION
+UNION 
 
 select product_id, 10 as price
 from products
 group by product_id
-having min(change_date) > '2019-08-16'
-
-
-
-
+having min(change_date) > '2019-08-16';
