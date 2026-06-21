c4aa9aa9ebd1ea5a861bcc8bc716deb431467c51
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-21 16:41:45 -0500

    Time: 1744 ms (16.03%), Space: 0B (100%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,18 +1,19 @@
 # Write your MySQL query statement below
-# need to show 0 if there is no count.
-
-select "Low Salary" as category,
-sum(CASE WHEN income < 20000 THEN 1 ELSE 0 END) as accounts_count
+select 'Low Salary' as category,
+count(*) as accounts_count
 from accounts
+where income < 20000
+
+UNION
+
+select 'Average Salary' as category,
+count(*) as accounts_count
+from accounts
+where income >= 20000 and income <= 50000
 
 UNION 
 
-select "Average Salary" as category,
-sum(CASE WHEN income >= 20000 and income <= 50000 THEN 1 ELSE 0 END) as accounts_count
+select 'High Salary' as category,
+count(*) as accounts_count
 from accounts
-
-UNION 
-
-select "High Salary" as category,
-sum(CASE WHEN income > 50000 THEN 1 ELSE 0 END) as accounts_count
-from accounts
+where income > 50000
