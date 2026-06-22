e8b84bb69fb55333393960528039a246b6bb31ea
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-21 23:11:40 -0500

    Time: 1249 ms (5.05%), Space: 0B (100%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,24 +1,23 @@
 # Write your MySQL query statement below
 
-# need to the total of each day
-with cte as
-(
-   select 
-    customer_id, 
-    name, 
-    visited_on, 
-    sum(amount) as dailyTotal
-from customer
-group by visited_on 
+# Analyze a possible expansion ( at least one customer every day)
+# moving average for seven days 
+
+# for the moving average, I need to calculate daily total first.
+
+with cte as (
+    select visited_on, sum(amount) as dailyTotal
+    from customer
+    group by visited_on 
 ),
 
 # need to get cumulated sum for 7 days to calculate moving average
-rolling as(
-select 
-    visited_on,
-    SUM(dailyTotal) OVER(order by visited_on rows between 6 preceding and current row ) as amount,
-    count(*) OVER (order by visited_on rows between 6 preceding and current row ) as days
-from cte
+
+rolling as (
+    select visited_on,
+    sum(dailyTotal) over(order by visited_on rows between 6 preceding and current row) as amount,
+    count(*) over (order by visited_on rows between 6 preceding and current row) as days
+    from cte
 )
 
 select 
@@ -27,3 +26,4 @@ select
     round(amount/7, 2) as average_amount
 from rolling
 where days = 7
+
