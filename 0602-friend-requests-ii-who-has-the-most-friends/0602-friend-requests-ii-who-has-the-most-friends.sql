c1a651bb1c84df0b2d1169e3f37d7890ff85552a
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-21 23:23:57 -0500

    Time: 349 ms (22.05%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,19 +1,14 @@
 # Write your MySQL query statement below
-
-# person who have the most friends and the most friends number
-
-# count(registered_id) group by accepter_id
-# count (accepter_id) group by requester_id
-
-with cte as
-(
-select requester_id as id from RequestAccepted
-union all
-select accepter_id as id from RequestAccepted
+with cte as(
+    select requester_id as id
+    from RequestAccepted
+    UNION ALL
+    select accepter_id as id
+    from RequestAccepted
 )
 
-SELECT id, COUNT(*) AS num
-FROM cte
-GROUP BY id
-ORDER BY num DESC
-LIMIT 1;
+select id, count(*) as num
+from cte
+group by id
+order by num desc
+limit 1
