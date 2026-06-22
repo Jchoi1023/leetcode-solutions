899705d2e91933953518229a8cc2330a3edf513c
Jihye Choi <118368645+Jchoi1023@users.noreply.github.com>
2026-06-21 22:54:02 -0500

    Time: 1245 ms (93.76%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,44 +1,35 @@
 # Write your MySQL query statement below
 
-# Need to find two differet things
-# The name of the user who has rated the greates number of movies
-# The movie name with the highest average rating in Feb 2020
+# find the name of the user who has rated the greatest number of movies
+# find the movie name with the highest average rating in Feb 2020.
 
-# First of all, I need to join all the table to get the data
-# Need separate queries for both because two different “top 1” problems with different grouping rules
-with cte as(
-select 
-    m.movie_id,
-    m.title,
-    u.user_id, 
-    u.name,
-    mr.rating,
-    mr.created_at
-from movies m 
-left join movierating mr on m.movie_id = mr.movie_id
-left join users u on mr.user_id = u. user_id
+with cte as (
+    select m.movie_id, m.title, u.user_id, u.name, mr.rating, mr.created_at
+    from movies m
+    left join movierating mr on m.movie_id = mr.movie_id
+    left join users u on u.user_id = mr.user_id
 ),
-
-user_result as (
-select name as results
+greatest_number as (
+  select name as results, count(*) as count
 from cte
-group by user_id
--- having count(*) 
-order by count(*) desc, name asc
+group by name 
+order by count desc, name asc
 limit 1
 ),
-
-movie_result as (
-    select title as results
-    from cte
-    where created_at between '2020-02-01' and '2020-02-29'
-    group by title
-    order by avg(rating) desc, title asc
-    limit  1
+highest_avg as (
+select title as results, avg(rating) as average
+from cte
+where created_at between '2020-02-01' and '2020-02-29'
+group by title
+order by average desc, title asc
+limit 1
 )
 
-select results from user_result
+
+select results from greatest_number
 
 UNION ALL
 
-select results from movie_result
+select results from highest_avg
+
+
